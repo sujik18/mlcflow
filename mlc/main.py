e25961089a81d16972d5d13c9cbe0115d050d269
Arjun Suresh <arjunsuresh1987@gmail.com>
2025-01-22 04:49:01 +0530

    Use meta.yaml for script meta

--- mlc/main.py
+++ mlc/main.py
@@ -354,7 +354,7 @@ class Action:
 
         # Save metadata
         meta_format = "yaml" if i.get("yaml") else "json"
-        item_meta_path = os.path.join(item_path, f"_cm.{meta_format}")
+        item_meta_path = os.path.join(item_path, f"meta.{meta_format}")
         if meta_format == "yaml":
             save_result = utils.save_yaml(item_meta_path, meta=item_meta)
         else:
@@ -415,7 +415,7 @@ class Action:
         for item in found_items:
             meta = {}
             # Load the current meta of the item
-            item_meta_path = os.path.join(item.path, "_cm.json")
+            item_meta_path = os.path.join(item.path, "meta.json")
             if os.path.exists(item_meta_path):
                 res = utils.load_json(item_meta_path)
                 if res['return']> 0:
@@ -486,26 +486,24 @@ class Action:
         else:
             result = res['list'][0]
             src_item_path = result.path
-            src_repo_path = result.repo_path
             src_item_meta = result.meta
 
 
         if len(target_split) > 1:
             target_repo = target_split[0]
             target_repo_path = os.path.join(self.repo_path, target_repo)
-            #target_repo_meta.path
+            target_repo = Repo(target_repo_path)
             target_item_name = target_split[1]
         else:
-            target_repo_path = src_repo_path  #use the src repo itself
-            target_repo_meta = result.repo_meta
+            target_repo = result.repo
             target_item_name = target_split[0]
 
         target_item_path = os.path.join(target_repo_path, action_target, target_item)
-        print(f"src_path = {src_item_path}, target_item = {target_item_name}, target_item_path = {target_item_path}, target_meta = {target_repo_meta}")
-        target_repo = Repo(target_repo_path, target_repo_meta)
+        print(f"src_path = {src_item_path}, target_item = {target_item_name}, target_item_path = {target_item_path}, target_repo = {target_repo}")
+        self.copy_item(src_path, target_item_path)
         target_item = Item(target_item_path, target_repo)
         return 1
-        self.copy_item_with_meta(src_path, target_item_path, src_item_meta)
+        self.copy_item(src_path, target_item_path)
 
         ii = {}
         res = self.save_new_meta(ii, item_id, target_item, action_target, target_item_path, repo_meta, target_repo_path)
@@ -513,7 +511,7 @@ class Action:
             return res
 
 
-    def copy_item_with_meta(self, source_path, destination_path):
+    def copy_item(self, source_path, destination_path):
         try:
             # Copy the source folder to the destination
             shutil.copytree(source_path, destination_path)
@@ -650,8 +648,8 @@ class Index:
                     if not os.path.isdir(automation_path):
                         continue
 
-                    # Check for configuration files (_cm.yaml or _cm.json)
-                    for config_file in ["_cm.yaml", "_cm.json"]:
+                    # Check for configuration files (meta.yaml or meta.json)
+                    for config_file in ["meta.yaml", "meta.json"]:
                         config_path = os.path.join(automation_path, config_file)
                         if os.path.isfile(config_path):
                             self._process_config_file(config_path, folder_type, automation_path, repo)
@@ -660,7 +658,7 @@ class Index:
 
     def _process_config_file(self, config_file, folder_type, folder_path, repo):
         """
-        Process a single configuration file (_cm.json or _cm.yaml) and add its data to the corresponding index.
+        Process a single configuration file (meta.json or meta.yaml) and add its data to the corresponding index.
 
         Args:
             config_file (str): Path to the configuration file.
@@ -729,8 +727,8 @@ class Item:
 
 
     def _load_meta(self):
-        yaml_file = os.path.join(self.path, "_cm.yaml")
-        json_file = os.path.join(self.path, "_cm.json")
+        yaml_file = os.path.join(self.path, "meta.yaml")
+        json_file = os.path.join(self.path, "meta.json")
 
         if os.path.exists(yaml_file):
             self.meta = utils.read_yaml(yaml_file)
@@ -775,8 +773,8 @@ class Automation:
         self._load_meta()
 
     def _load_meta(self):
-        yaml_file = os.path.join(self.path, "_cm.yaml")
-        json_file = os.path.join(self.path, "_cm.json")
+        yaml_file = os.path.join(self.path, "meta.yaml")
+        json_file = os.path.join(self.path, "meta.json")
 
         if os.path.exists(yaml_file):
             self.meta = utils.read_yaml(yaml_file)
